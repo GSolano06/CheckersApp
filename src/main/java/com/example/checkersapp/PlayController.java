cd7192a7d1c3b95d955a2cdc0ca5e96da67c2a5e
shoebsadi <shoebmsadi@gmail.com>
2025-05-06 13:36:01 -0400

    sxdcfgh

--- src/main/java/com/example/checkersapp/PlayController.java
+++ src/main/java/com/example/checkersapp/PlayController.java
@@ -2,13 +2,123 @@ package com.example.checkersapp;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
 
 public class PlayController {
-    @FXML
-    private Label welcomeText;
+//all rectangles
+    public Rectangle a8;
+    public Rectangle a7;
+    public Rectangle a6;
+    public Rectangle a5;
+    public Rectangle a4;
+    public Rectangle a3;
+    public Rectangle a2;
+    public Rectangle a1;
+    public Rectangle b7;
+    public Rectangle b8;
+    public Rectangle b5;
+    public Rectangle b6;
+    public Rectangle b3;
+    public Rectangle b4;
+    public Rectangle b1;
+    public Rectangle b2;
+    public Rectangle c8;
+    public Rectangle c7;
+    public Rectangle c6;
+    public Rectangle c5;
+    public Rectangle c4;
+    public Rectangle c3;
+    public Rectangle c2;
+    public Rectangle c1;
+    public Rectangle d7;
+    public Rectangle d8;
+    public Rectangle d5;
+    public Rectangle d6;
+    public Rectangle d3;
+    public Rectangle d4;
+    public Rectangle d1;
+    public Rectangle d2;
+    public Rectangle e8;
+    public Rectangle e7;
+    public Rectangle e6;
+    public Rectangle e5;
+    public Rectangle e4;
+    public Rectangle e3;
+    public Rectangle e2;
+    public Rectangle e1;
+    public Rectangle f7;
+    public Rectangle f8;
+    public Rectangle f5;
+    public Rectangle f6;
+    public Rectangle f3;
+    public Rectangle f4;
+    public Rectangle f1;
+    public Rectangle f2;
+    public Rectangle g8;
+    public Rectangle g7;
+    public Rectangle g6;
+    public Rectangle g5;
+    public Rectangle g4;
+    public Rectangle g3;
+    public Rectangle g2;
+    public Rectangle g1;
+    public Rectangle h7;
+    public Rectangle h8;
+    public Rectangle h5;
+    public Rectangle h6;
+    public Rectangle h3;
+    public Rectangle h4;
+    public Rectangle h1;
+    public Rectangle h2;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    //all circles
+    public Circle l1;
+    public Circle l5;
+    public Circle l9;
+    public Circle l6;
+    public Circle l10;
+    public Circle l2;
+    public Circle l7;
+    public Circle l3;
+    public Circle l11;
+    public Circle l8;
+    public Circle l4;
+    public Circle l12;
+    public Circle z9;
+    public Circle z1;
+    public Circle z5;
+    public Circle z10;
+    public Circle z2;
+    public Circle z6;
+    public Circle z11;
+    public Circle z3;
+    public Circle z7;
+    public Circle z12;
+    public Circle z4;
+    public Circle z8;
+
+//all labels
+public Label Label8;
+    public Label Label7;
+    public Label Label6;
+    public Label Label5;
+    public Label Label4;
+    public Label Label3;
+    public Label Label2;
+    public Label Label1;
+    public Label Labela;
+    public Label Labelb;
+    public Label Labelc;
+    public Label Labeld;
+    public Label Labele;
+    public Label Labelf;
+    public Label Labelg;
+    public Label Labelh;
+
+
+
+
+
 }
